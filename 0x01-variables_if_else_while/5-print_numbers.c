9cb4a9053e4d36f93530df275cac62a03590867a
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-08-11 16:47:46 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,18 +3,18 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single digit numbers of base 10
+ * Description: Prints all single-digit numbers of base 10
  *              starting from 0, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int number = 0;
-while (number <= 9){
-putchar(number + '0');
-number++;
+int number;
+for (number = 0; number <= 9; number++)
+{
+printf("%d", number);
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
