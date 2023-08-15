3530fcb32988e7ab6496341c134194c65a6f4735
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-08-15 14:44:03 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,29 +11,22 @@ if (n >= 0 && n <= 15)
 {
 for (num = 0; num <= n; num++)
 {
+_putchar('0');
 for (mult = 0; mult <= n; mult++)
-{
-prod = num * mult;
-if (mult != 0)
 {
 _putchar(',');
 _putchar(' ');
-}
-if (prod <= 9 && mult != 0)
-{
+prod = num * mult;
+if (prod <= 99)
 _putchar(' ');
-_putchar(' ');
-_putchar(' ');
-}
-else if (prod <= 99 && mult != 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
 if (prod >= 100)
-_putchar((prod / 100) + '0');
-if (prod >= 10)
-_putchar(((prod / 10) % 10) + '0');
+{
+_putchar((prod / 010) + '0');
+_putchar(((prod / 10) % 10 + '0');
+}
+else if (prod <= 99 && mult >= 10)
+{
+_putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
