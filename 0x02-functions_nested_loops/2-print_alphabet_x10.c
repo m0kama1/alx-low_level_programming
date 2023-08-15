a0eef352797adc7298723738b63af22b85b1fc18
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-08-15 02:21:50 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 char ch;
 int i;
 i = 0;
-while (i<10)
+while (i < 10)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {
@@ -16,5 +16,4 @@ _putchar(ch);
 }
 _putchar('\n');
 i++;
-}
 }
