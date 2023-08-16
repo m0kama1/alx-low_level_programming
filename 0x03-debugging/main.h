4bc23d47eeab70a12b2f21af20fe8dd49b7cf858
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-08-16 03:13:22 -0700

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Other includes and declarations */
-
-int convert_day(int month, int day);  /* Declaration of convert_day function */
-void print_remaining_days(int month, int day, int year);  /* Declaration of .. */
-/* Rest of the header file */
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 #endif /* MAIN_H */
+
+
+
+
+
+
