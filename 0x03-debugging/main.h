7217b166ff33b86e2a884a3d16a64cf96c707640
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-08-16 01:32:35 -0700

    Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,10 +4,6 @@
 /* Other includes and declarations */
 
 int convert_day(int month, int day);  /* Declaration of convert_day function */
-void print_remaining_days(int month, int day, int year);  /* Declaration of print_remaining_days function */
-
+void print_remaining_days(int month, int day, int year);  /* Declaration of .. */
 /* Rest of the header file */
-
 #endif /* MAIN_H */
-
-
