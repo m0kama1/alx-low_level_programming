1575faa98633a122b947f1f50073301f6c321954
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-08-16 04:35:52 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,15 @@
 
 int main(void)
 {
-	int i,z = 0;
-
-	while(i < 1024)
-	{
-	if ((i % 3 == 0) || (i % 5 == 0))
-	{
-	z += i;
-	}
-	i++;
-	}
-	printf("%d\n", z);
-	return(z);
+int i,z = 0;
+while(i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += i;
+}
+i++;
+}
+printf("%d\n", z);
+return(0);
 }
