f2ee5342fc1a45d69d1a97b33704bf6b0024ece7
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-08-23 11:08:03 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,24 +8,21 @@
 
 void rev_string(char *n)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
+int i = 0;
+int j = 0;
+char temp;
+while (*(n + i) != '\0')
+{
+i++;
 }
-
+i--;
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+}
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -34,7 +31,6 @@ void rev_string(char *n)
  * @size_r: buffer size
  * Return: pointer to calling function
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int overflow = 0, i = 0, j = 0, digits = 0;
