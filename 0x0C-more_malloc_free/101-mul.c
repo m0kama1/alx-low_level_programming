3b5c3e687812cc77cb118d0381e8ed156c126406
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-09-07 04:53:56 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,7 +13,6 @@
 int is_digit(char *s)
 {
 int i = 0;
-
 while (s[i])
 {
 if (s[i] < '0' || s[i] > '9')
@@ -22,7 +21,6 @@ i++;
 }
 return (1);
 }
-
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
@@ -32,9 +30,8 @@ return (1);
 int _strlen(char *s)
 {
 int i = 0;
-
 while (s[i] != '\0')
-	{
+{
 i++;
 }
 return (i);
