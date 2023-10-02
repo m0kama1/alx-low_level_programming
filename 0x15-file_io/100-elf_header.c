eec9ba7021fbd2ffc697b058ee76e921c3a88c1f
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-10-02 13:39:20 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -26,6 +26,7 @@ void close_elf(int elf);
 void check_elf(unsigned char *e_ident)
 {
 int index;
+
 for (index = 0; index < 4; index++)
 {
 if (e_ident[index] != 127 &&
@@ -38,6 +39,7 @@ exit(98);
 }
 }
 }
+
 /**
  * print_magic - Prints the magic numbers of an ELF header.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
@@ -47,23 +49,28 @@ exit(98);
 void print_magic(unsigned char *e_ident)
 {
 int index;
-printf(" Magic: ");
+
+printf("  Magic:   ");
+
 for (index = 0; index < EI_NIDENT; index++)
 {
 printf("%02x", e_ident[index]);
+
 if (index == EI_NIDENT - 1)
 printf("\n");
 else
 printf(" ");
 }
 }
+
 /**
  * print_class - Prints the class of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
  */
 void print_class(unsigned char *e_ident)
 {
-printf(" Class: ");
+printf("  Class:                             ");
+
 switch (e_ident[EI_CLASS])
 {
 case ELFCLASSNONE:
@@ -79,13 +86,15 @@ default:
 printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 }
 }
+
 /**
  * print_data - Prints the data of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
  */
 void print_data(unsigned char *e_ident)
 {
-printf(" Data: ");
+printf("  Data:                              ");
+
 switch (e_ident[EI_DATA])
 {
 case ELFDATANONE:
@@ -103,13 +112,14 @@ printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 }
 
 /**
- *  * print_version - Prints the version of an ELF header.
- *   * @e_ident: A pointer to an array containing the ELF version.
- *    */
+ * print_version - Prints the version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
+ */
 void print_version(unsigned char *e_ident)
 {
-printf(" Version: %d",
+printf("  Version:                           %d",
 e_ident[EI_VERSION]);
+
 switch (e_ident[EI_VERSION])
 {
 case EV_CURRENT:
@@ -120,14 +130,14 @@ printf("\n");
 break;
 }
 }
+
 /**
  * print_osabi - Prints the OS/ABI of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
  */
 void print_osabi(unsigned char *e_ident)
 {
-printf(" OS/ABI: ");
-
+printf("  OS/ABI:                            ");
 switch (e_ident[EI_OSABI])
 {
 case ELFOSABI_NONE:
@@ -171,7 +181,7 @@ printf("<unknown: %x>\n", e_ident[EI_OSABI]);
  */
 void print_abi(unsigned char *e_ident)
 {
-printf(" ABI Version: %d\n",
+printf("  ABI Version:                       %d\n",
 e_ident[EI_ABIVERSION]);
 }
 
@@ -184,7 +194,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 e_type >>= 8;
-printf(" Type: ");
+printf("  Type:                              ");
 switch (e_type)
 {
 case ET_NONE:
@@ -206,6 +216,7 @@ default:
 printf("<unknown: %x>\n", e_type);
 }
 }
+
 /**
  * print_entry - Prints the entry point of an ELF header.
  * @e_entry: The address of the ELF entry point.
@@ -213,18 +224,22 @@ printf("<unknown: %x>\n", e_type);
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-printf(" Entry point address: ");
+printf("  Entry point address:               ");
+
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 {
 e_entry = ((e_entry << 8) & 0xFF00FF00) |
 ((e_entry >> 8) & 0xFF00FF);
 e_entry = (e_entry << 16) | (e_entry >> 16);
 }
+
 if (e_ident[EI_CLASS] == ELFCLASS32)
 printf("%#x\n", (unsigned int)e_entry);
+
 else
 printf("%#lx\n", e_entry);
 }
+
 /**
  * close_elf - Closes an ELF file.
  * @elf: The file descriptor of the ELF file.
@@ -240,16 +255,17 @@ dprintf(STDERR_FILENO,
 exit(98);
 }
 }
+
 /**
  * main - Displays the information contained in the
- * ELF header at the start of an ELF file.
+ *        ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
  * Description: If the file is not an ELF File or
- * the function fails - exit code 98.
+ *              the function fails - exit code 98.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
@@ -286,6 +302,7 @@ print_osabi(header->e_ident);
 print_abi(header->e_ident);
 print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
+
 free(header);
 close_elf(o);
 return (0);
