b0a28eb882fcb06f4e077e6794ff263ffda21e0e
m0kama1 <m.kamal.abdulaah@gmail.com>
2023-10-02 13:29:53 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -110,7 +110,6 @@ void print_version(unsigned char *e_ident)
 {
 printf(" Version: %d",
 e_ident[EI_VERSION]);
-
 switch (e_ident[EI_VERSION])
 {
 case EV_CURRENT:
@@ -121,7 +120,6 @@ printf("\n");
 break;
 }
 }
-
 /**
  * print_osabi - Prints the OS/ABI of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
@@ -186,9 +184,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 e_type >>= 8;
-
 printf(" Type: ");
-
 switch (e_type)
 {
 case ET_NONE:
@@ -210,7 +206,6 @@ default:
 printf("<unknown: %x>\n", e_type);
 }
 }
-
 /**
  * print_entry - Prints the entry point of an ELF header.
  * @e_entry: The address of the ELF entry point.
@@ -219,21 +214,17 @@ printf("<unknown: %x>\n", e_type);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
 printf(" Entry point address: ");
-
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 {
 e_entry = ((e_entry << 8) & 0xFF00FF00) |
 ((e_entry >> 8) & 0xFF00FF);
 e_entry = (e_entry << 16) | (e_entry >> 16);
 }
-
 if (e_ident[EI_CLASS] == ELFCLASS32)
 printf("%#x\n", (unsigned int)e_entry);
-
 else
 printf("%#lx\n", e_entry);
 }
-
 /**
  * close_elf - Closes an ELF file.
  * @elf: The file descriptor of the ELF file.
@@ -249,7 +240,6 @@ dprintf(STDERR_FILENO,
 exit(98);
 }
 }
-
 /**
  * main - Displays the information contained in the
  * ELF header at the start of an ELF file.
@@ -265,7 +255,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 Elf64_Ehdr *header;
 int o, r;
-
 o = open(argv[1], O_RDONLY);
 if (o == -1)
 {
@@ -287,7 +276,6 @@ close_elf(o);
 dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 exit(98);
 }
-
 check_elf(header->e_ident);
 printf("ELF Header:\n");
 print_magic(header->e_ident);
@@ -298,7 +286,6 @@ print_osabi(header->e_ident);
 print_abi(header->e_ident);
 print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
-
 free(header);
 close_elf(o);
 return (0);
